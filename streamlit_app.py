1e3f635e26fa3db30b3d2903d49d84e4bbe7490f
libsdu <138094012+libsdu@users.noreply.github.com>
2024-11-13 21:21:09 -0700

    Update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -14,7 +14,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie wil be:', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -42,6 +43,5 @@ if time_to_insert:
     
     st.success(f'✅Your Smoothie is ordered, {name_on_order}!')
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+
 
