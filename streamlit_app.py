cdda22b4d8599f8e9db35ac11aca2dd263928102
libsdu <138094012+libsdu@users.noreply.github.com>
2024-11-13 21:10:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -41,6 +39,10 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     
     st.success(f'✅Your Smoothie is ordered, {name_on_order}!')
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 
 cnx = st.connection("snowflake")
 session = cnx.session()
